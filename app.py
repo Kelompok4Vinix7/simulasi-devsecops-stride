f0c7765b1e1cf3d93f78f6fb0e9a22d856302c44
Kelompok4Vinix7 <kelompok4vinix7@gmail.com>
2026-06-25 03:07:02 +0000

    tes gitleaks

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 AWS_ACCESS_KEY_ID = "AKIAIMZAMB1AEXAMPLE1"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
-
 DATABASE_URL = "postgres://admin:p4ssw0rd_rahasia_banget@db.example.com:5432/mydb"
 
 def connect():
@@ -10,3 +9,4 @@ def connect():
 
 if __name__ == "__main__":
     connect()
+# tes gitleaks
