aaec40d665c132945ab85bd59aa1f00a5ce47bde
Kelompok4Vinix7 <kelompok4vinix7@gmail.com>
2026-06-25 02:47:58 +0000

    commit app dengan credencial code

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+AWS_ACCESS_KEY_ID = "AKIAIMZAMB1AEXAMPLE1"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+
+DATABASE_URL = "postgres://admin:p4ssw0rd_rahasia_banget@db.example.com:5432/mydb"
+
+def connect():
+    print("Menghubungkan ke AWS...")
+
+if __name__ == "__main__":
+    connect()
